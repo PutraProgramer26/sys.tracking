28f749cd9e786a0c9862e8c0c7d6fc97311e620f
PutraProgramer26 <putrarisna26@gmail.com>
2026-09-13 20:33:34 +0900

    update Buka PDF

--- shipping-document.php
+++ shipping-document.php
@@ -55,7 +55,7 @@ if ($isPdf) {
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
     <?= $documentStyles; ?>
   </head>
-  <body>
+  <body class="<?= $isPdf ? 'pdf-document' : ''; ?>">
     <main class="main-panel reservation-page document-only-page">
       <div class="reservation-document">
         <header class="reservation-header">
